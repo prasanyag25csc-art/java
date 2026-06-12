bdfbb7896701765775523d715ef3e91f85f15ade
prasanyag25csc-art <prasanyag25csc@vetias.ac.in>
2026-06-12 13:34:15 +0530

    Day7

--- src/Day7.java
+++ src/Day7.java
@@ -28,18 +28,57 @@
         }
         static class
     }
-}*/
+*/
 
-public  class Day7 {
-    static class Request {
+public class Day7 {
+    static class Request{
         String name;
         String date;
         String reason;
 
-        Request(String name, String date, String reason) {
+        Request(String name, String date, String reason){
             this.name = name;
             this.date = date;
             this.reason = reason;
         }
+
+        void approve() {
+            System.out.println("Approving request");
+        }
+        void reject() {
+            System.out.println("Reject request");
+        }
+    }
+
+    static class LeaveRequest extends Request {
+
+        LeaveRequest(String name, String date, String reason) {
+            super(name, date, reason);
+        }
+        void approveLeaveRequest() {
+            System.out.println("Approving leave request");
+        }
+        void rejectLeaveRequest() {
+            System.out.println("Reject leave request");
+        }
+    }
+    static class OnDutyRequest extends Request {
+
+        OnDutyRequest(String name, String date, String reason) {
+            super(name, date, reason);
+        }
+        void approveOnDutyRequest() {
+            System.out.println("Approving on duty request");
+        }
+        void rejectOnDutyRequest() {
+            System.out.println("Reject on duty request");
+        }
+    }
+
+    static void main() {
+        LeaveRequest leaveRequest = new LeaveRequest("Menaga","12.06.2026","cold");
+        leaveRequest.approveLeaveRequest();
+        OnDutyRequest onDutyRequest = new OnDutyRequest("Menaga","11.07.2026","Culturals");
+        onDutyRequest.rejectOnDutyRequest();
     }
 }
