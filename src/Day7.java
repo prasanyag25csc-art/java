64f7a61559a219d261e43ce32e75f7073195e7e3
prasanyag25csc-art <prasanyag25csc@vetias.ac.in>
2026-06-12 11:53:33 +0530

    day

--- src/Day7.java
+++ src/Day7.java
@@ -10,9 +10,33 @@ public class Day7{
         LeaveRequest(String to){
             super(to);
         }
-        void sendLeaveRequest(){
-            System.out.println("");
+        public class Day7{
+            static class request{
+                public String to;
+                request(String to){
+                    this.to=to;
+                }
+                void send(){
+                }
+            }static class LeaveRequest extends request {
+                LeaveRequest(String to){
+                    super(to);
+                }
+                void sendLeaveRequest(){
+                    System.out.println("Name:");
+                    System.out.println("Date:");
+                    System.out.println("Reason:");
+                }
+                static class OnDutyRequest(){
+                    OnDutyRequest(String to){
+                        super(to);
+                    }
+                    void OnDutyRequest(){
+                        System.out.println("Name:");
+                        System.out.println("Date:");
+                        System.out.println("Request:");
+                    }
+                }
+                static class
+            }
         }
-        void  sendleaverequest
-    }
-}
