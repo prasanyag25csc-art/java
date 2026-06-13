f858280d63d7162ee8424c91dcee48cf3db2fc8c
prasanyag25csc-art <prasanyag25csc@vetias.ac.in>
2026-06-13 14:40:18 +0530

    today

---
+++ src/day1.java
@@ -0,0 +1,29 @@
+public class day1 {
+    /*void main(){
+    int n=5;
+    for(int row=1;row<=5;row++){
+        for(int column=1;column<=row;column++){
+            System.out.print(column);
+        }
+            System.out.println();
+    }
+}*/
+
+    void main() {
+        int[] marks = {87, 32, 44, 89, 23};
+        System.out.println("pass mark");
+        for (int mark : marks) {
+            if (mark > 40) {
+                System.out.println(mark + "");
+            }
+        }
+        System.out.println("pass mark");
+        for (int mark : marks) {
+            if (mark > 40) {
+                System.out.println(mark + "");
+
+            }
+        }
+    }
+
+}
