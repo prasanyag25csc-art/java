d127aaa645baf7cf9166288cbc41d3076f7985d4
prasanya <prasanyag25csc@vetias.ac.in>
2026-06-16 10:56:08 +0530

    day10

---
+++ src/day10.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class day10 {
+    static void main(){
+        String[] Student={"aishu","pras","gopz","ijk","mon"};
+        ArrayList<String>list= new ArrayList();
+        list.add("aishu");
+        list.add("pras");
+        list.add("gopz");
+        list.add("ijk");
+        list.add("mon");
+        for(String s: list) {
+            System.out.println(s);
+    }
+        list.remove("mon");
+        System.out.println();
+        for(String s: list) {
+            System.out.println(s);
+
+        }
+
+
+}}
