488985225b0a39758a879367b2b76ea704fc6e7d
prasanya <prasanyag25csc@vetias.ac.in>
2026-06-17 16:14:20 +0530

    day11

--- src/day11.java
+++ src/day11.java
@@ -1,4 +1,5 @@
-import java.util.HashSet;
+
+/*import java.util.HashSet;
 import java.util.Set;
 public class day11 {
     public static void main(String[] args) {
@@ -11,5 +12,18 @@ public class day11 {
         for(String S:set){
         System.out.println(S);
     }
-}
+  }
+} */
+import java.util.HashMap;
+import java.util.HashSet;
+public class day11 {
+    public static void main(String[] args) {
+        HashMap<String, Object> person = new HashMap<>();
+        person.put("ram", "89");
+        person.put("john", "78");
+        person.put("abc","15");
+        person.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
 }
